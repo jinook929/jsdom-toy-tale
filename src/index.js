ea10fc89832e3337b269ba5995a96a8b6852be5c
jinook929 <jinook929@gmail.com>
2021-04-12 23:53:07 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,85 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  const addToyForm = document.querySelector(".add-toy-form");
+  addToyForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        name: addToyForm.elements.name.value,
+        image: addToyForm.elements.image.value,
+        likes: 0
+      })
+    })
+    .then(res => res.json())
+    .then(obj => {
+      addCard(obj);
+      addToyForm.elements.name.value = "";
+      addToyForm.elements.image.value = "";
+    })
+    .catch(error => {
+      console.log(error.message);
+    });
+  });
+
+  getToys();
 });
+
+function getToys() {
+  const toy_collection = document.querySelector("#toy-collection");
+  toy_collection.innerHTML = "";
+  fetch("http://localhost:3000/toys")
+  .then(res => res.json())
+  .then(obj => {
+    obj.forEach(toy => {
+      addCard(toy);
+    });
+  });
+}
+
+function addCard(toy) {
+  const toy_collection = document.querySelector("#toy-collection");
+  const card = document.createElement("div")
+  card.setAttribute("class", "card");
+  const h2 = document.createElement("h2");
+  const img = document.createElement("img");
+  const p = document.createElement("p");
+  const button = document.createElement("button");
+  h2.textContent = toy.name;
+  card.appendChild(h2);
+  img.classList.add("toy-avatar");
+  img.src = toy.image;
+  card.appendChild(img);
+  p.textContent = `${toy.likes} Likes `;
+  card.appendChild(p);
+  button.classList.add("like-btn");
+  button.textContent = "Like <3";
+  button.addEventListener("click", (e) => {
+    incrementLikes(toy, p)
+  });
+  card.appendChild(button);
+  toy_collection.appendChild(card);
+}
+
+function incrementLikes(toy, p) {
+  fetch(`http://localhost:3000/toys/${toy.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      likes: toy.likes + 1
+    })
+  })
+  .then(res => res.json())
+  .then(obj => {
+    p.textContent = `${toy.likes+1} Likes `;
+  })
+}
